83a85e6e2e8b9d29c50da58f0a8ad3d204ad5c46
Philipp Haussleiter <philipp@haussleiter.de>
2014-09-16 21:22:42 +0200

    this fixes #11  Add link to issue tracker

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,13 +1,17 @@
 package controllers;
 
+import com.typesafe.config.ConfigFactory;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.Application.*;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render());
-    }
+	public final static String ISSUE_TRACKER_URL = ConfigFactory.load().getString("issue.tracker.url");
+
+	public static Result index() {
+		return ok(index.render());
+	}
 
 }
