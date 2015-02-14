7dfc58e9c3bb68fe8ff44f651af32477e3835d59
Philipp Haussleiter <philipp@haussleiter.de>
2015-02-15 00:31:13 +0100

    add 10 mins Cache for Statistics

--- app/controllers/RoomStatistics.java
+++ app/controllers/RoomStatistics.java
@@ -6,18 +6,27 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import models.openfire.Room;
+import play.cache.Cache;
 import play.libs.Json;
 import play.mvc.Result;
 
 public class RoomStatistics extends Application {
+
+	private final static int CACHE_TTL_IN_MINUTES = 60 * 10;
 
 	public static Result jsonShow(Long id, Integer days) {
 		Room room = Room.Finder.byId(id);
 		if (room == null) {
 			return notFound("room with id " + id + " not found!");
 		}
+		String cacheKey = "roomStats-" + id;
 		ObjectNode result = Json.newObject();
-		result.put("entries", collectEntries(room, days, result.arrayNode()));
+		ArrayNode results = (ArrayNode) Cache.get(cacheKey);
+		if (results == null) {
+			results = collectEntries(room, days, result.arrayNode());
+			Cache.set(cacheKey, results, CACHE_TTL_IN_MINUTES);
+		}
+		result.put("entries", results);
 		return ok(result);
 	}
 
